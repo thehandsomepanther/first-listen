5c3aa3d872b872eee6f061ff76974e8f2c7466cc
Josh Shi <jshi@C02RC3N0G8WM.group.on>
2016-09-01 22:33:39 -0700

    still getting there

--- script.js
+++ script.js
@@ -1,49 +1,81 @@
-var tracks, responseUrl;
+var slimer = require('node-slimerJs')
+var request = require('request')
+var Xvfb = require('xvfb')
+var xvfb = new Xvfb({'silent': true, 'displayNumber': 200})
 
-var system = require('system');
-var page = require('webpage').create();
-var jQuery = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+var JQUERY = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'
 
-page.onError = function(msg, trace) {
-  console.log("ERROR: " + JSON.stringify(trace, null, 2));
-};
+var DEFAULT_URL = 'http://www.npr.org/series/98679384/first-listen'
+var url = process.argv[2] ? process.argv[2] : DEFAULT_URL
 
-page.onResourceReceived = function(response) {
-  if (response.contentType == "audio/mpeg") {
-    if (!responseUrl) {
-      responseUrl = response.url;
-      getTracks();
-    }
-  }
-};
+if (!url.match('^http:\/\/www\.npr\.org(.*)first-listen(.*)$')) {
+  console.log("That's not an NPR First Listen link, but here's what's available:")
+  url = DEFAULT_URL
+}
 
-page
-  .open(system.args[1])
-  .then(function(getTracks) {
-    page.includeJs(jQuery, function() {
-      tracks = page.evaluate(function() {
-        return $('article.playlistitem').length;
+if (url === DEFAULT_URL) {
+  request(url, function(err, res, body) {
+    if (err) throw err
+  })
+}
+
+url = 'http://www.npr.org/2016/09/01/491941924/first-listen-st-paul-the-broken-bones-sea-of-noise'
+
+tracks = request(url, function() {
+
+})
+
+var getBaseUrl = function(url) {
+  var trackUrl = false
+
+  slimer.create(function(err, sl) {
+    return sl.createPage(function(err, page) {
+      return page.open(url, function(err, status) {
+        page.onResourceReceived = function(response) {
+          if (response.contentType == "audio/mpeg" && !trackUrl) {
+            trackUrl = response.url;
+          }
+        }
+
+        page.includeJs(JQUERY, function(err) {
+          if (err) throw 'jQuery couldn\'t be loaded'
+          return page.evaluate(function() {
+            $($('article.playlistitem')[0]).find('button.audio-module-listen').click()
+          }, function(err, result) {
+            sleep(4000).then(() => {
+              page.close()
+            })
+          })
+        })
+
+        sleep(4000).then(() => {
+          sl.exit()
+        })
       })
-      page.evaluate(function() {
-        window.setTimeout(function() {
-          $($('article.playlistitem')[0]).find('button.audio-module-listen').click();
-        }, 1000);
-      });
-    });
-    setTimeout(function() {
-      slimer.exit();
-    }, 5000);
-  });
+    })
+  }, {slimerPath: require('slimerjs').path})
+
+  return new Promise((resolve) => setTimeout(function(){ resolve(trackUrl) }, 5000))
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 var pad = function(n, width) {
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
 }
 
-var getTracks = function() {
+var getTracks = function(responseUrl) {
   baseUrl = responseUrl.substring(0, responseUrl.indexOf("?")-6);
-  for (var i = 1; i < tracks+1; i++) {
+  for (var i = 1; i < 13; i++) {
     var url = baseUrl + pad(i, 2) + ".mp3";
-    console.log(url);
+    request(url).pipe(url)
   }
 }
+
+baseUrlPromise = getBaseUrl(url);
+baseUrlPromise.then((url) => {
+  console.log(url)
+})
