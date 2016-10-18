335a6f1cbf7449fc1341920d1d66aedc8750d38a
Joshua Shi <thehandsomepanther@gmail.com>
2016-10-18 18:40:29 -0500

    support song titles

--- script.sh
+++ script.sh
@@ -1,13 +1,24 @@
-url="https://ondemand.npr.org/anon.npr-mp3/npr/fl/2016/09/";
-filebase="20160914_fl_rs";
+url="https://ondemand.npr.org/anon.npr-mp3/npr/fl/2016/10/";
+filebase="20161011_fl_a";
 
-artist="Ryuichi Sakamoto";
-album="Nagasaki: Memories Of My Son";
+artist="American Football";
+album="American Football (LP2)";
 year="2016";
-total="28";
+total="9";
+declare -a tracks=(
+	"Where Are We Now?" 
+	"My Instincts Are The Enemy" 
+	"Home Is Where The Haunt Is" 
+	"Born To Lose" 
+	"I've Been So Lost For So Long" 
+	"Give Me The Gun" 
+	"I Need a Drink (Or Two or Three)" 
+	"Desire Gets in the Way" 
+	"Everyong Is Dressed Up" 
+)
 
 for i in `seq 01 $total`;
 do 
 	curl -OL $url$filebase`printf "%02d" $i`.mp3;
-	id3tag --artist="$artist" --album="$album" --year=$year --track=$i --total=$total $filebase`printf "%02d" $i`.mp3;
+	id3tag --artist="$artist" --album="$album" --year=$year --track=$i --total=$total --song="${tracks[$i-1]}" $filebase`printf "%02d" $i`.mp3;
 done;
