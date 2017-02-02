051e8830743fad2f6351ee9ce9921c3f765b7fc9
Josh Shi <thehandsomepanther@gmail.com>
2017-02-02 15:10:50 -0600

    update

--- readme.md
+++ readme.md
@@ -5,9 +5,10 @@ this was mostly an exercise in attempting to scrape a website which uses flash t
 1. hit the page of the album you'd like to scrape, obtain info like artist, album title, album cover, song titles, album length, etc
 2. click the play button to fire a network call which fetches the mp3 for the selected song
 3. listen for the call that has a media data type, and grab that url
-4. iterate over that url and make a raw get request for each song 
+4. iterate over that url and make a raw get request for each song
 5. (optional) use an id3 tagging library like id3v2 to edit metadata, since npr strips all that information
 
 unfortunately, i haven't been able to find a good web driver that supports automation on flash websites
 
 `script.sh` is an example of steps 4 and 5
+the script uses [id3lib](https://github.com/attilagyorffy/id3lib-osx). you can get it through homebrew with `brew install id3lib`
--- script.sh
+++ script.sh
@@ -1,24 +1,25 @@
-url="https://ondemand.npr.org/anon.npr-mp3/npr/fl/2016/10/";
-filebase="20161011_fl_a";
+url="https://ondemand.npr.org/anon.npr-mp3/npr/fl/2017/01/";
+filebase="20170131_fl_td";
 
-artist="American Football";
-album="American Football (LP2)";
-year="2016";
-total="9";
+artist="Teen Daze";
+album="Themes for Dying Earth";
+year="2017";
+total="10";
 declare -a tracks=(
-	"Where Are We Now?" 
-	"My Instincts Are The Enemy" 
-	"Home Is Where The Haunt Is" 
-	"Born To Lose" 
-	"I've Been So Lost For So Long" 
-	"Give Me The Gun" 
-	"I Need a Drink (Or Two or Three)" 
-	"Desire Gets in the Way" 
-	"Everyong Is Dressed Up" 
+	"Cycle"
+	"Dream City"
+	"Becoming"
+	"Lost (feat. Nadia Hulett)"
+	"Cherry Blossoms (feat. Dustin Wong)"
+	"First Rain (feat. S. Carey)"
+	"Rising (feat. Sound of Ceres)"
+	"Anew (feat. Jon Anderson)"
+	"Water in Heaven"
+	"Breath"
 )
 
 for i in `seq 01 $total`;
-do 
-	curl -OL $url$filebase`printf "%02d" $i`.mp3;
-	id3tag --artist="$artist" --album="$album" --year=$year --track=$i --total=$total --song="${tracks[$i-1]}" $filebase`printf "%02d" $i`.mp3;
+do
+	curl -OL $url$filebase`printf $i`.mp3;
+	id3tag --artist="$artist" --album="$album" --year=$year --track=$i --total=$total --song="${tracks[$i-1]}" $filebase`printf $i`.mp3;
 done;
